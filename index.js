f160c1a3757d1d7c478671b251f970e1c01e0270
Lisa Mao <liisamao@gmail.com>
2018-08-06 23:18:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution in this file.
+function lowerCaseDrivers(drivers){
+  return drivers.map(driver => driver.toLowerCase());
+};
+
+function splitToObj (driver){
+  let splitDri= driver.split(" "); 
+  return {firstName: splitDri[0], lastName: splitDri[1]};
+};
+
+function nameToAttributes(drivers){
+  return drivers.map(driver => splitToObj(driver));
+};
+
+function attributesToPhrase(drivers){
+  return drivers.map(desc => desc.name + " is from " + desc.hometown);
+}
